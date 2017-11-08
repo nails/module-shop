2a9b99104c04385819bafa733e55a5c9703d2747
Pablo de la Peña <p@shedcollective.org>
2017-11-08 20:34:48 +0000

    Using PDODatabase

--- src/Routes.php
+++ src/Routes.php
@@ -24,7 +24,7 @@ class Routes implements RouteGenerator
      */
     public static function generate()
     {
-        $oDb     = Factory::service('ConsoleDatabase', 'nailsapp/module-console');
+        $oDb     = Factory::service('PDODatabase');
         $oModel  = Factory::model('AppSetting');
         $aRoutes = [];
 
