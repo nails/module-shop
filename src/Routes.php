611e723deff86e1f0066592e703cf6c27427a317
Pablo de la Peña <p@shedcollective.org>
2017-11-08 20:34:24 +0000

    Reverted back to ConsoleDatabase

--- src/Routes.php
+++ src/Routes.php
@@ -24,7 +24,7 @@ class Routes implements RouteGenerator
      */
     public static function generate()
     {
-        $oDb     = Factory::service('PDODatabase');
+        $oDb     = Factory::service('ConsoleDatabase', 'nailsapp/module-console');
         $oModel  = Factory::model('AppSetting');
         $aRoutes = [];
 
